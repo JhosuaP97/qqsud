e27f38365c8f227431b76aec3b99273111a544b5
=Jhosua Pachon <jhosuapachon@gmail.com>
2020-05-11 10:59:33 -0500

    Updates

---
+++ precache-manifest.852613ac842801646ca217eb710aeff1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07be1b61f41942966aba65557dae294b",
+    "url": "/qqsud/index.html"
+  },
+  {
+    "revision": "81a45c047c334f1e9014",
+    "url": "/qqsud/static/css/main.47261a0e.chunk.css"
+  },
+  {
+    "revision": "aea4d99785e58b6e913a",
+    "url": "/qqsud/static/js/2.86c15b68.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/qqsud/static/js/2.86c15b68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81a45c047c334f1e9014",
+    "url": "/qqsud/static/js/main.39f1efe8.chunk.js"
+  },
+  {
+    "revision": "2b5a6630fad8f3199372",
+    "url": "/qqsud/static/js/runtime-main.a8bfa413.js"
+  }
+]);
